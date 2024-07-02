031d0cd45547bd82d1ba13f2f7c57b9ea3991aeb
secret1739 <teamsecret466@gmail.com>
2024-07-03 00:52:31 +0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,9 @@
+AOS.init(); 
+// This adds some nice ellipsis to the description:
+document.querySelectorAll(".projcard-description").forEach(function(box) {
+	$clamp(box, {clamp: 6});
+});
+// This adds some nice ellipsis to the description:
+document.querySelectorAll(".projcard-description").forEach(function(box) {
+	$clamp(box, {clamp: 6});
+});
